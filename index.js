98fc8bb1fbdd0685aed924a0f9fbb60d3cd0e8de
nar2p1nk <farrisisqandar@gmail.com>
2022-02-10 14:28:13 +0800

    installed socket.io and socket.io-client and initialize socket.oi in index.js, listening for connection

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express();
 const http = require('http');
-const server = http.createServer(app)
+const httpServer = http.createServer(app)
+const {Server} = require('socket.io');
+
+const io = new Server(httpServer);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -10,6 +13,11 @@ app.get('/',(req,res) =>{
     res.sendFile(__dirname + '/index.html')
 });
 
-server.listen(8080, ()=>{
+io.on('connection',socket=>{
+    console.log('user connected');
+});
+
+
+httpServer.listen(8080, ()=>{
     console.log('server listening on: localhost:8080')
 })
